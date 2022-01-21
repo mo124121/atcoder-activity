b4fde74bf6bb13cc6338f1f6201b04f84eb31c93
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-01-22 08:44:48 +0900

    binominialのクラス追加

--- common/copype.py
+++ common/copype.py
@@ -96,6 +96,30 @@ def gcd(x, y):
         return gcd(y, x % y)
 
 
+class Binominal:
+    def __init__(self, N, mod) -> None:
+        fact = [1, 1]
+        factinv = [1, 1]
+        inv = [0, 1]
+
+        for i in range(2, N + 1):
+            fact.append((fact[-1] * i) % mod)
+            inv.append((-inv[mod % i] * (mod // i)) % mod)
+            factinv.append((factinv[-1] * inv[-1]) % mod)
+
+        self.fact = fact
+        self.factinv = factinv
+        self.inv = inv
+        self.mod = mod
+        self.N = N
+
+    def calc(self, n, r):
+        if r < 0 or n < r:
+            return 0
+        r = min(r, n - r)
+        return self.fact[n] * self.factinv[r] * self.factinv[n - r] % self.mod
+
+
 if __name__ == "__main__":
     linkedlist = LinkedList()
 
