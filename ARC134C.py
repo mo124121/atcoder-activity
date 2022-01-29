a2074d693a9350732f7cb11fd7a4a069c07a3dcf
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-01-29 23:04:47 +0900

    これだと割り算でおかしくなってとけない

--- ARC134C.py
+++ ARC134C.py
@@ -4,43 +4,29 @@ A = list(map(int, input().split()))
 MOD = 998244353
 rest = sum(A[1:])
 
-if rest > A[0] - K:
+if rest + K > A[0]:
     print(0)
     exit()
 
+A[0] -= rest + K
 
-class Binominal:
-    def __init__(self, N, mod) -> None:
-        fact = [1, 1]
-        factinv = [1, 1]
-        inv = [0, 1]
+dp = [[0] * (N) for _ in range(K + 1)]
 
-        for i in range(2, N + 1):
-            fact.append((fact[-1] * i) % mod)
-            inv.append((-inv[mod % i] * (mod // i)) % mod)
-            factinv.append((factinv[-1] * inv[-1]) % mod)
+for i in range(N):
+    dp[1][i] = 1
 
-        self.fact = fact
-        self.factinv = factinv
-        self.inv = inv
-        self.mod = mod
-        self.N = N
-
-    def calc(self, n, r):
-        if r < 0 or n < r:
-            return 0
-        r = min(r, n - r)
-        return self.fact[n] * self.factinv[r] * self.factinv[n - r] % self.mod
-
+for k in range(1, K):
+    for i in range(N):
+        dp[k + 1][i] = ((dp[k][i] * (A[i] + k))) % MOD
 
 ret = 1
-binom = Binominal(MOD, MOD)
-ret *= binom.calc(A[0] - K - rest + K - 1, K - 1)
-ret % MOD
+div = 1
+for i in range(1, K):
+    div *= i
+div %= MOD
 
-
-for i in range(1, N):
-    ret *= binom.calc(A[i] + K - 1, K - 1)
+for i in range(N):
+    ret *= dp[K][i] // div
     ret %= MOD
 
 print(ret)
