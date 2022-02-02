b45081c41fc469ca25b7c5706b98e850bfb2d436
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-02-03 07:05:14 +0900

    倍数の性質を理解すべし

--- typical90/042/main.py
+++ typical90/042/main.py
@@ -5,6 +5,18 @@ MOD = 1000000007  # type: int
 
 
 def solve(K: int):
+    if K % 9 != 0:
+        print(0)
+        return
+    dp = [0] * (K + 1)
+    dp[0] = 1
+    for i in range(K + 1):
+        B = min(i, 9)
+        for j in range(1, B + 1):
+            dp[i] += dp[i - j]
+            dp[i] %= MOD
+    print(dp[K])
+
     return
 
 
@@ -14,9 +26,11 @@ def main():
         for line in sys.stdin:
             for word in line.split():
                 yield word
+
     tokens = iterate_tokens()
     K = int(next(tokens))  # type: int
     solve(K)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
