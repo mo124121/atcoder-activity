a620d1880147f8bd12b79094803bc42d1f7bc0e7
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-02-27 23:29:33 +0900

    B別解

--- ARC136B.py
+++ ARC136B.py
@@ -1,71 +1,24 @@
-from collections import defaultdict, deque
-
-
 N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-flag = False
-q = deque()
-seen = defaultdict(int)
-for i in range(N):
-    q.append(A[i])
-    if A[i] in seen:
-        flag = True
-    seen[A[i]] += 1
 
-total = 0
-for i in range(N - 3):
-    b = B[i]
-    if seen[b] == 0:
-        print("No")
-        exit()
-    seen[b] -= 1
-    bufs = []
-    while len(q):
-        v = q.popleft()
-        if v == b:
-            if len(bufs):
-                dist = len(bufs)
-                for j in range(dist - 2):
-                    q.appendleft(bufs.pop())
-                if len(bufs) == 2:
-                    if dist % 2 == 0:
-                        q.appendleft(bufs[1])
-                        q.appendleft(bufs[0])
-                    else:
-                        q.appendleft(bufs[0])
-                        q.appendleft(bufs[1])
-                else:
-                    tmp = q.popleft()
-                    q.appendleft(bufs[0])
-                    q.appendleft(tmp)
-            break
-        else:
-            bufs.append(v)
-
-if len(q) != 3:
+def No():
     print("No")
     exit()
 
-buf = []
-for i in range(3):
-    buf.append(q.popleft())
-    b = B[-3 + i]
-    if seen[b] == 0:
-        print("No")
-        exit()
-    seen[b] -= 1
 
-for i in range(3):
-    if (
-        buf[(0 + i) % 3] == B[-3]
-        and buf[(1 + i) % 3] == B[-2]
-        and buf[(2 + i) % 3] == B[-1]
-    ):
-        flag = True
+if sorted(A) != sorted(B):
+    No()
 
-if flag:
-    print("Yes")
+if len(set(A)) == N:
+    cnt = 0
+    for i in range(N):
+        cnt += A.index(B[i])
+        A.remove(B[i])
+    if cnt % 2 == 0:
+        print("Yes")
+    else:
+        No()
 else:
-    print("No")
+    print("Yes")
