41d9edfc8b95f0776401b2e0417737ebba95da7f
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-03-29 06:47:56 +0900

    確率の線形性

--- typical90/066/main.py
+++ typical90/066/main.py
@@ -3,6 +3,17 @@ import sys
 
 
 def solve(N: int, L: "List[int]", R: "List[int]"):
+    ret = 0
+    for i in range(1, N):
+        for j in range(i):
+            count = 0
+            for k in range(L[j], R[j] + 1):
+                for l in range(L[i], R[i] + 1):
+                    if l < k:
+                        count += 1
+            ret += count / ((R[j] - L[j] + 1) * (R[i] - L[i] + 1))
+    print(ret)
+
     return
 
 
@@ -12,6 +23,7 @@ def main():
         for line in sys.stdin:
             for word in line.split():
                 yield word
+
     tokens = iterate_tokens()
     N = int(next(tokens))  # type: int
     L = [int()] * (N)  # type: "List[int]"
@@ -21,5 +33,26 @@ def main():
         R[i] = int(next(tokens))
     solve(N, L, R)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
+
+
+"""
+考察
+期待値？モンテカルロ？（小声）
+
+N<100
+L<=R<100
+制約は小さめ->N^3 あたりを狙いたくなる
+
+これもdpくさい
+要素が増えるごとの期待値を計算していくイメージ？
+
+そもそも転倒数とは何か？
+LISの降順,LDS　違う　最長である必要はない
+全組み合わせの個数なら、過去のとる値の範囲×今回採用する値の範囲で適当に計算したらええんちゃうの？
+
+
+
+"""
