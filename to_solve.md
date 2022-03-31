bb7c247147723c12e0ac4e22b80b1eb4f77bcedb
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-04-01 06:47:26 +0900

    2次元いもす　解くものリストの追加

---
+++ to_solve.md
@@ -0,0 +1,9 @@
+# 私的解くべきリスト
+
+## 木DP
+
+- 典型90問 073 https://atcoder.jp/contests/typical90/tasks/typical90_bu
+- ABC070-D
+- ABC138-D
+- ABC187-E
+- ABC149-F
--- typical90/081/main.py
+++ typical90/081/main.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
 import sys
 
+MAX = 5000
+
 
 def solve(N: int, K: int, A: "List[int]", B: "List[int]"):
+    MAX = max(max(A), max(B))
+
+    dp = [[0] * (MAX + 2) for _ in range(MAX + 2)]
+
+    for i in range(N):
+        a_min = A[i]
+        a_max = min(A[i] + K, MAX) + 1
+        b_min = B[i]
+        b_max = min(B[i] + K, MAX) + 1
+
+        dp[a_min][b_min] += 1
+        dp[a_min][b_max] -= 1
+        dp[a_max][b_min] -= 1
+        dp[a_max][b_max] += 1
+
+    ret = 0
+    for i in range(MAX + 2):
+        for j in range(MAX + 1):
+            dp[i][j + 1] += dp[i][j]
+    for j in range(MAX + 2):
+        for i in range(MAX + 1):
+            dp[i + 1][j] += dp[i][j]
+            ret = max(ret, dp[i][j])
+
+    print(ret)
+
     return
 
 
@@ -12,6 +40,7 @@ def main():
         for line in sys.stdin:
             for word in line.split():
                 yield word
+
     tokens = iterate_tokens()
     N = int(next(tokens))  # type: int
     K = int(next(tokens))  # type: int
@@ -22,5 +51,33 @@ def main():
         B[i] = int(next(tokens))
     solve(N, K, A, B)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
+
+
+"""
+考察
+N<2*10^5
+K<5000
+N^2はきつい、KNあたりも微妙か
+
+ソートして尺取り法っぽくはある、ただ2次元だとわからん
+1次元なら尺取りで解ける
+身長で尺取りして、その範囲でソートして尺取りするとどうなる？
+ソートO(NlogN) + 身長尺取りO(N) * (ソートO(NlogN)*体重尺取りO(N)）=O(N^2logN)
+
+multiset出てきそう…
+
+発想を変える
+x:身長、y:体重で各人をプロットした時、K x Kの箱を置くときに一番たくさん入る置き方、みたいな問題
+とすると、ある人がx,yとすると、[x-K,x+K],[y-K,y+K]の範囲はその人がチームに入れる
+つまり全員の範囲を合算して、一番大きいのを見つければ良い。　O(N+K^2)
+すなわち2次元いもす、勝った。実装を忘れたこと以外は…
+
+いもすの考え方が違いそう、上だと幅が広すぎる
+[x,x+K][y,y+K]にしようか
+sample AC, test WA
+
+
+"""
