5d2220b096955bf98db107693e28a73aa8b793f7
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-04-11 00:11:19 +0900

    upsolveできない

--- other/ABC247F.py
+++ other/ABC247F.py
@@ -1,12 +1,42 @@
+from collections import deque
+from functools import lru_cache
+
+
 N = int(input())
 P = list(map(int, input().split()))
 Q = list(map(int, input().split()))
 pair = {}
 MOD = 998244353
 
+
+@lru_cache(maxsize=None)
+def luca(i):
+    if i == 1:
+        return 2
+    if i == 2:
+        return 3
+    return luca(i - 1) + luca(i - 2)
+
+
+seen = {}
 for i in range(N):
     pair[P[i]] = Q[i]
 
+ls = []
+st = list(range(1, N + 1))
+ret = 1
+while len(st):
+    i = st.pop()
+    if i in seen:
+        continue
+    l = 0
+    while i not in seen:
+        seen[i] = True
+        i = pair[i]
+        l += 1
+    ret *= luca(l)
+    ret %= MOD
+print(ret)
 
 """
 考察だけでもする
