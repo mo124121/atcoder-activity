fb3972b2c6123d549c12fd09509f12975b0b306e
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-04-16 15:21:41 +0900

    最大値にやられる

--- dwacon5th-prelimsB.py
+++ dwacon5th-prelimsB.py
@@ -1,37 +1,28 @@
-from heapq import heappop, heappush
-
-
 N, K = map(int, input().split())
 a = list(map(int, input().split()))
 
 beauty = [[0] * N for _ in range(N)]
-h = []
+vs = []
 for i in range(N):
     beauty[i][i] = a[i]
-    heappush(h, -a[i])
+    vs.append(a[i])
 
-for l in range(N):
+for l in range(N - 1):
     for r in range(l + 1, N):
         beauty[l][r] = beauty[l][r - 1] + a[r]
-        heappush(h, -beauty[l][r])
+        vs.append(beauty[l][r])
 
-l = []
+
 ret = 0
-for i in reversed(range(31)):
+for i in reversed(range(40)):
     ret_i = ret + (1 << i)
-    while len(h):
-        v = -heappop(h)
-        if v < 1 << i:
-            heappush(h, -v)
-            break
-        l.append(v)
+
     r = []
-    for li in l:
-        if (li & ret_i) == ret_i:
-            r.append(li)
+    for v in vs:
+        if (v & ret_i) == ret_i:
+            r.append(v)
 
     if len(r) >= K:
-        l = r
         ret = ret_i
 
 
@@ -46,4 +37,6 @@ N<10**3
 bitの重なりが一番残るやつを選ぶ感じ
 上のビットからK個以上たってる集合を順次選んでいくイメージ
 
+教訓
+最大値がどのあたりに来るかちゃんと考察すべき
 """
