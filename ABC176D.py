f21a29a493e69e9960c1718860b14db3fcd3303f
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-05 05:04:24 +0900

    あさかつ

--- ABC176D.py
+++ ABC176D.py
@@ -12,48 +12,44 @@ Dw -= 1
 S = [list(input()) for _ in range(H)]
 INF = 10**6
 cost = [[INF] * W for _ in range(H)]
-cost[Ch][Cw] = 0
-he = [(0, Ch, Cw)]
 
-mvs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-while len(he):
-    while len(he):
-        c, h, w = heappop(he)
+q = [(0, Ch, Cw)]
+q2 = []
+mvs = [[0, 0], [1, 0], [0, 1], [-1, 0], [0, -1]]
+
+while len(q):
+    while len(q):
+        c, h, w = q.pop()
+        q2.append((c, h, w))
         if (h, w) == (Dh, Dw):
             print(c)
             exit()
+        if c > cost[h][w]:
+            continue
         # 徒歩
         for mv in mvs:
             nh = h + mv[0]
             nw = w + mv[1]
             if 0 <= nh < H and 0 <= nw < W:
-                if S[nh][nw] != "#" and cost[nh][nw] > c:
-                    heappush(he, (c, nh, nw))
+                if S[nh][nw] != "#" and cost[nh][nw] == INF:
+                    q.append((c, nh, nw))
                     cost[nh][nw] = c
-    # ワープ元登録
-    for nh in range(H):
-        for nw in range(W):
-            if cost[nh][nw] == c:
-                heappush(he, (c, nh, nw))
-    c_prev = c
     # ワープ
-    while len(he):
-        c, h, w = heappop(he)
-        if c == c_prev + 1:
-            heappush(he, (c, h, w))
+    while len(q2):
+        c, h, w = q2.pop()
         c += 1
         for x in range(-2, 3):
             for y in range(-2, 3):
-                if abs(x) + abs(y) < 2:
-                    continue
                 nh = h + y
                 nw = w + x
                 if 0 <= nh < H and 0 <= nw < W:
-                    if S[nh][nw] != "#" and cost[nh][nw] > c:
-                        heappush(he, (c, nh, nw))
+                    if S[nh][nw] != "#" and cost[nh][nw] == INF:
+                        q.append((c, nh, nw))
                         cost[nh][nw] = c
 
+
 print(-1)
+
 """
 幅探索亜種
 
@@ -62,6 +58,9 @@ HW<10**6
 遷移探索範囲 25
 
 ワープ数をheapで管理しておくのが良さそう
+
+TLEだしちょっと戦略を変える
+
 
 
 """
