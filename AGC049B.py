fd8ccea18c4bb9340f01f635f86a09a2d67f94bb
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-07 10:21:12 +0900

    みどりマラソン

--- AGC049B.py
+++ AGC049B.py
@@ -4,11 +4,32 @@ T = list(map(int, list(input())))
 
 
 r = 1
+ret = 0
 for l in range(N):
-    if S[l] != T[l]:
+    if r <= l:
+        r = l + 1
+    if S[l] == 1 and T[l] == 0:
         while r < N:
-
+            if S[r] == 1:
+                S[l] = 0
+                S[r] = 0
+                ret += r - l
+                break
             r += 1
+        r += 1
+    elif S[l] == 0 and T[l] == 1:
+        while r < N:
+            if S[r] == 1:
+                S[l] = 1
+                S[r] = 0
+                ret += r - l
+                break
+            r += 1
+        r += 1
+if S == T:
+    print(ret)
+else:
+    print(-1)
 
 
 """
@@ -24,6 +45,11 @@ for l in range(N):
 尺取り法っぽい
 
 左のほうに埋めていく
+
+16/34 WA
+考慮が抜けてる系
+
+AC
 
 
 """
