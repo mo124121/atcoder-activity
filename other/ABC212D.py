af03f765345a07c2099287fd44f7f3202e4d16cd
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-09 06:02:52 +0900

    整理

--- other/ABC212D.py
+++ other/ABC212D.py
@@ -2,15 +2,19 @@ from heapq import heappop, heappush
 
 
 Q = int(input())
-a = 0
+
 h = []
 ret = []
+a = 0
 for i in range(Q):
-    t = list(map(int, input().split()))
-    if t[0] == 1:
-        heappush(h, t[1] - a)
-    elif t[0] == 2:
-        a += t[1]
-    else:
+    q = input()
+    if q[0] == "3":
         ret.append(heappop(h) + a)
+    else:
+        x = int(q[2:])
+        if q[0] == "1":
+            heappush(h, x - a)
+        else:
+            a += x
+
 print(*ret, sep="\n")
