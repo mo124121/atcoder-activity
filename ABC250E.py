310a606a9bdccd3325fbb5fedc7e95566f3fcb86
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-08 23:48:19 +0900

    Zobrist Hash履修

--- ABC250E.py
+++ ABC250E.py
@@ -1,69 +1,36 @@
-from bisect import bisect, bisect_left
-
+from random import randint
 
 N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-seen_b = set()
-b_sizes = []
-for b in B:
-    seen_b.add(b)
-    b_sizes.append(len(seen_b))
-
-ok = dict()
-seen_a = set()
-seen_b = set()
-a_not_b = set()
-b_not_a = set()
-skip_count = 0
-for i_a, a in enumerate(A):
-    seen_a.add(a)
-    if a not in seen_b:
-        a_not_b.add(a)
-    a_size = len(seen_a)
-    b_i = bisect_left(b_sizes, a_size)
-    b_j = bisect(b_sizes, a_size)
-    if b_i < N:
-        b = B[b_i]
-        seen_b.add(b)
-        if b not in seen_a:
-            b_not_a.add(b)
-
-    if skip_count > 0:
-        skip_count -= 1
-        continue
-
-    del_set = set()
-    for a_k in a_not_b:
-        if a_k in seen_b:
-            del_set.add(a_k)
-    a_not_b.difference_update(del_set)
-
-    del_set = set()
-    for b_k in b_not_a:
-        if b_k in seen_a:
-            del_set.add(b_k)
-    b_not_a.difference_update(del_set)
-
-    if len(a_not_b) == 0 and len(b_not_a) == 0:
-        ok[i_a + 1] = (b_i + 1, b_j + 1)
-    else:
-        skip_count = len(a_not_b) - 1
+R_map = {}
+for v in set(A + B):
+    R_map[v] = randint(0, 10**18)
 
 
-Q = int(input())
+def hs(A):
+    seen = set()
+    s = 0
+    r = [0]
+    for a in A:
+        if a not in seen:
+            seen.add(a)
+            s ^= R_map[a]
+        r.append(s)
+    return r
+
+
+Ah = hs(A)
+Bh = hs(B)
+
 ret = []
+Q = int(input())
 for i in range(Q):
     x, y = map(int, input().split())
-    if x not in ok:
-        ret.append("No")
-        continue
-    l, r = ok[x]
-    if l <= y < r:
+    if Ah[x] == Bh[y]:
         ret.append("Yes")
     else:
         ret.append("No")
-
 print(*ret, sep="\n")
 
 """
@@ -88,5 +55,8 @@ N対Nのマッチングをとる問題
 
 17WA/34
 悪くはないはず
+
+zobrist hashとな
+
 
 """
