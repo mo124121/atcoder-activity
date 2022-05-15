b0d65ba2682a2244ecba78d60c1f2816020e403b
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-15 12:19:54 +0900

    up solve

--- ABC251D.py
+++ ABC251D.py
@@ -1,29 +1,15 @@
 W = int(input())
 
-A = [i + 1 for i in range(2**7 - 1)]
+A = [i + 1 for i in range(100)]
 
-B = [(i + 1) << 7 for i in range(2**6 - 1)]
-C = [(i + 1) << 13 for i in range(2**6 - 1)]
+B = [(i + 1) * 10**2 for i in range(100)]
+C = [(i + 1) * 10**4 for i in range(100)]
 
 
 A = A + B + C
 N = len(A)
 print(N)
 print(*A)
-
-ret = set()
-for i in range(N):
-    ret.add(A[i])
-    for j in range(N):
-        if i == j:
-            continue
-        ret.add(A[i] + A[j])
-        for k in range(N):
-            if i == k or j == k:
-                continue
-            ret.add(A[i] + A[j] + A[k])
-
-print(ret)
 
 """
 逆に1,Wに到達可能な組み合わせを考える
--- ABC251E.py
+++ ABC251E.py
@@ -1,19 +1,22 @@
 INF = 10**18
+
 N = int(input())
 A = [INF] + list(map(int, input().split()))
 
 dp = [INF] * (N + 1)
-dp[0] = A[0]
+dp[1] = dp[2] = A[1]
 
-for i in range(1, N + 1):
-    dp[i] = min(dp[i - 1], dp[i - 2]) + A[i - 1]
+for i in range(3, N + 1):
+    dp[i] = min(min(dp[i - 2], dp[i - 1]) + A[i - 1], dp[i - 1] + A[i])
+
 ret = dp[-1]
 
 dp = [INF] * (N)
 dp[0] = dp[1] = A[-1]
 
 for i in range(2, N):
-    dp[i] = min(dp[i - 1], dp[i - 2]) + A[i - 1]
+    dp[i] = min(min(dp[i - 1], dp[i - 2]) + A[i - 1], dp[i - 1] + A[i])
+
 ret = min(ret, dp[-1])
 
 print(ret)
