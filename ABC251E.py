cbbbb5f95fcc17bfaf690ae949038b837b38d323
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-15 12:42:09 +0900

    解説解

--- ABC251E.py
+++ ABC251E.py
@@ -3,20 +3,21 @@ INF = 10**18
 N = int(input())
 A = [INF] + list(map(int, input().split()))
 
-dp = [INF] * (N + 1)
-dp[1] = dp[2] = A[1]
+dp = [[INF] * 2 for _ in range(N + 1)]
+dp[1][0] = 0
+for i in range(2, N + 1):
+    dp[i][0] = dp[i - 1][1]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][1]) + A[i]
+ret = dp[N][1]
 
-for i in range(3, N + 1):
-    dp[i] = min(min(dp[i - 2], dp[i - 1]) + A[i - 1], dp[i - 1] + A[i])
-
-ret = dp[-1]
-
-dp = [INF] * (N)
-dp[0] = dp[1] = A[-1]
-
-for i in range(2, N):
-    dp[i] = min(min(dp[i - 1], dp[i - 2]) + A[i - 1], dp[i - 1] + A[i])
-
-ret = min(ret, dp[-1])
-
+dp = [[INF] * 2 for _ in range(N + 1)]
+dp[1][1] = A[1]
+for i in range(2, N + 1):
+    dp[i][0] = dp[i - 1][1]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][1]) + A[i]
+ret = min(ret, *dp[N])
 print(ret)
+"""
+dp[i][j]:i個目目までの行動で、j=0行動しない、j=1行動する場合の最小コスト
+
+"""
