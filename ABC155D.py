18e5c5272b7bd532801001711164525316b525d8
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-19 17:11:21 +0900

    途中

--- ABC155D.py
+++ ABC155D.py
@@ -1,5 +1,37 @@
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
+A.sort()
+l = -(10**18) - 1
+r = 10**18 + 1
+
+
+def count(x):
+    k = 0
+    for a in A:
+        l = 0
+        r = len(A)
+        while r - l > 1:
+            m = (r + l) // 2
+            if a * A[m] < x:
+                l = m
+            else:
+                r = m
+        if a * a < a * A[l]:
+            k -= 1
+        k += l
+
+    return k
+
+
+while r - l > 1:
+    m = (r + l) // 2
+    if count(m) < K:
+        l = m
+    else:
+        r = m
+
+print(l)
+
 
 """
 計算ができれば楽勝だが、
