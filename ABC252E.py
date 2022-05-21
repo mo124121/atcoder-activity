8a8d0afdd78cce424809f0493064f174e91e1579
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-22 07:01:52 +0900

    改善終了

--- ABC252E.py
+++ ABC252E.py
@@ -1,8 +1,10 @@
-from collections import defaultdict
 from heapq import heappop, heappush
+import sys
+
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
-G = defaultdict(list)
+G = [[] for _ in range(N + 1)]
 
 for i in range(M):
     a, b, c = map(int, input().split())
@@ -14,24 +16,25 @@ d = 0
 node = 1
 e = 0
 parent = 0
-heappush(h, (d, node, e))
-ret = []
+heappush(h, (d, node))
+ret = [-1] * (N + 1)
 INF = 10**18
 dist = [INF] * (N + 1)
 
 while h:
-    d, node, e = heappop(h)
+    d, node = heappop(h)
 
-    if dist[node] <= d:
+    if dist[node] < d:
         continue
-    dist[node] = d
-    ret.append(e)
 
-    for nxt, c, en in G[node]:
-        if dist[nxt] > d + c:
-            heappush(h, (d + c, nxt, en))
+    for nxt, c, e in G[node]:
+        nd = d + c
+        if dist[nxt] > nd:
+            dist[nxt] = nd
+            ret[nxt] = e
+            heappush(h, (nd, nxt))
 
-print(*ret[1:])
+print(*ret[2:])
 
 """
 最小全域木でいいか？
