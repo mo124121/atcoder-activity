13790b5b00b5aeeba24252bfc3e227dc71024916
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-23 17:18:58 +0900

    dpで書き換え

--- ABC252D.py
+++ ABC252D.py
@@ -6,13 +6,11 @@ A = list(map(int, input().split()))
 count = Counter(A)
 M = len(count)
 B = list(count.values())
-s = sum(B)
-left = 0
-right = s - B[0]
-ret = 0
-for i in range(1, M - 1):
-    left += B[i - 1]
-    right -= B[i]
-    ret += left * B[i] * right
+dp = [[0] * 4 for _ in range(M + 1)]
 
-print(ret)
+for i in range(M):
+    dp[i][0] = 1
+    for b in range(3):
+        dp[i + 1][b + 1] = dp[i][b] * B[i] + dp[i][b + 1]
+
+print(dp[M][3])
