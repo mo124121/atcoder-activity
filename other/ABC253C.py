a170f100b937ace8098fa677f101b73ff0222995
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-05-29 13:32:45 +0900

    heap解法

--- other/ABC253C.py
+++ other/ABC253C.py
@@ -1,18 +1,29 @@
-from array import array
-from bisect import bisect_left, bisect_right, insort
+from collections import Counter
+from heapq import heappop, heappush
+
 
 Q = int(input())
-S = array("i")
-
+hs = []
+hl = []
+count = Counter()
 for _ in range(Q):
     q = list(map(int, input().split()))
     if q[0] == 1:
-        insort(S, q[1])
+        x = q[1]
+        heappush(hs, x)
+        heappush(hl, -x)
+        count[x] += 1
     elif q[0] == 2:
-        r = bisect_right(S, q[1])
-        l = bisect_left(S, q[1])
-        c = r - l
-        if c != 0:
-            del S[l : l + min(c, q[2])]
+        x = q[1]
+        c = q[2]
+        count[x] = max(0, count[x] - c)
     else:
-        print(S[-1] - S[0])
+        while count[hs[0]] == 0:
+            heappop(hs)
+        while count[-hl[0]] == 0:
+            heappop(hl)
+        print(-hl[0] - hs[0])
+
+"""
+heap解法
+"""
