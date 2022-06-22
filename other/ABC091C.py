35a048902612dc809335a8eaa2a350ccb30a0a70
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-06-22 20:48:58 +0900

    networkx

--- other/ABC091C.py
+++ other/ABC091C.py
@@ -8,21 +8,22 @@ AB = []
 for i in range(N):
     AB.append(tuple(map(int, input().split())))
     G.add_node(f"r{i}")
-    G.add_edge("s", f"r{i}")
+    G.add_edge("s", f"r{i}", capacity=1)
 CD = []
 for i in range(N):
     G.add_node(f"b{i}")
-    G.add_edge(f"b{i}", "t")
+    G.add_edge(f"b{i}", "t", capacity=1)
     CD.append(tuple(map(int, input().split())))
 
 
 for i, (a, b) in enumerate(AB):
     for j, (c, d) in enumerate(CD):
         if a < c and b < d:
-            G.add_edge(f"r{i}", f"b{j}")
+            G.add_edge(f"r{i}", f"b{j}", capacity=1)
 
-result = nx.ford_
+result = nx.maximum_flow_value(G, "s", "t")
 
+print(result)
 
 """
 マッチングの問題っぽいがその手の知識はない
