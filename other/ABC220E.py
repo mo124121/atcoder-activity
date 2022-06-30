82def96ff95cad7ba1c9231eac6976ec60bfcc97
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-06-30 17:34:07 +0900

    水

--- other/ABC220E.py
+++ other/ABC220E.py
@@ -1,37 +1,22 @@
-from functools import lru_cache
-import sys
-
-sys.setrecursionlimit(10**6 + 2)
-
-if sys.implementation.name == "pypy":
-    import pypyjit
-
-    pypyjit.set_param("max_unroll_recursion=-1")
-
 N, D = map(int, input().split())
 MOD = 998244353
 
 
-@lru_cache(maxsize=None)
-def root_path(n, d):
-    if n == 0:
-        return 0
-    l = n
-    r = d - l
-    ret = 0
-    if 0 <= r:
-        ret += pow(2, max(0, l - 1), MOD) * pow(2, max(r - 1, 0), MOD) % MOD
+f = [0] * (N + 1)
+g = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    l = i - 1
+    r = D - l
+    leaf = 0
+    if 0 <= r <= i - 1:
+        leaf = pow(2, l - 1, MOD) * pow(2, max(0, r - 1), MOD)
         if l != r:
-            ret = ret * 2 % MOD
-    ret += root_path(n - 1, d)
-    return ret
+            leaf *= 2
+        leaf %= MOD
+    g[i] = (g[i - 1] + leaf) % MOD
 
+for i in range(1, N + 1):
+    f[i] = (f[i - 1] * 2 + g[i]) % MOD
 
-def rec(n, d):
-    if n == 1:
-        return 0
-    ret = (rec(n - 1, d) * 2 + root_path(n, d)) % MOD
-    return ret
-
-
-print(rec(N, D) * 2 % MOD)
+print(f[N] * 2 % MOD)
