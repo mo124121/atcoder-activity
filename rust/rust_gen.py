6db1b8d09bcb9d78b29d9fbfd66ec97f2ccef710
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-07-06 07:24:34 +0900

    rustデビュー

---
+++ rust/rust_gen.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# https://github.com/paruma/atcoder_rust/blob/main/contest.py
+# example
+# ./contest.py abc206 b c
+
+import sys
+import os
+import shutil
+
+if len(sys.argv) <= 2:
+    print("usage  : ./contest.py [contest_name] [task_name_list]", file=sys.stderr)
+    print("example: ./contest.py abc206 b c", file=sys.stderr)
+    sys.exit(1)
+
+
+contest_name: str = sys.argv[1]
+problems = sys.argv[2:]
+
+RUST_PATH = "rust"
+# ソースコードの準備(ディレクトリの生成)
+dir_path: str = f"{RUST_PATH}/src/contest/{contest_name}/"
+if not os.path.exists(dir_path):
+    os.mkdir(dir_path)
+
+# ソースコードの準備(ファイルのコピー)
+templete_file_path: str = f"{RUST_PATH}/src/contest/template.rs"
+for problem in problems:
+    dst_file_path = f"{RUST_PATH}/src/contest/{contest_name}/main{problem}.rs"
+    if not os.path.exists(dst_file_path):
+        shutil.copy(templete_file_path, dst_file_path)
+
+
+# cargo.tomlコードの出力
+
+for problem in problems:
+    print(
+        f"""[[bin]]
+name = "{contest_name}_{problem}"
+path = "src/contest/{contest_name}/main{problem}.rs"
+    """
+    )
---
+++ rust/src/contest/template.rs
@@ -0,0 +1,19 @@
+#![allow(clippy::let_unit_value)]
+use proconio::input;
+
+fn read() {
+    input! {
+        //from OnceSource::from(""),
+    }
+}
+
+fn solve() {}
+
+fn output() {}
+
+fn main() {
+    let () = read();
+    let _ = solve();
+    output();
+    //println!("{}", ans);
+}
