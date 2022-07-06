44f11d83b3f90e747eaea59df8e8c69febebcb98
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-07-06 21:00:11 +0900

    abs

--- rust/src/contest/arc065/arc065_a.rs
+++ rust/src/contest/arc065/arc065_a.rs
@@ -4,11 +4,27 @@ use proconio::input;
 
 fn main() {
     input! {
-        S:String
+        mut S:String
     }
+    let mut l = S.len();
+    let CAND = ["dream", "dreamer", "erase", "eraser"];
     loop {
-        for s in ["dream", "dreamer", "erase", "eraser"] {
-            if &S[S.]
+        for s in CAND.iter() {
+            if S.len() < s.len() {
+                continue;
+            }
+            let i = S.len() - s.len();
+            if &S[i..] == *s {
+                S = S[..i].to_string();
+            }
         }
+        if S.is_empty() {
+            println!("YES");
+            return;
+        } else if S.len() == l {
+            println!("NO");
+            return;
+        }
+        l = S.len();
     }
 }
