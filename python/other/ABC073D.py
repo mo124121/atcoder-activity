c6f8097050588dff0b33e0e0dc516ab9bf1e0710
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-07-16 06:24:51 +0900

    scipy使う

--- python/other/ABC073D.py
+++ python/other/ABC073D.py
@@ -1,28 +1,15 @@
 from itertools import permutations
-
+from scipy.sparse import *
 
 N, M, R = map(int, input().split())
 r = list(map(int, input().split()))
-INF = 10**18
-wf = [[INF] * (N) for _ in range(N)]
 
+t = []
 for i in range(M):
     a, b, c = map(int, input().split())
-    a -= 1
-    b -= 1
-    wf[a][b] = c
-    wf[b][a] = c
+    t.append((a, b, c))
 
+a, b, c = zip(*t)
 
-for k in range(N):
-    for i in range(N):
-        for j in range(N):
-            wf[i][j] = min(wf[i][j], wf[i][k] + wf[k][j])
-
-ret = INF
-for pat in permutations(r):
-    a = 0
-    for i in range(R - 1):
-        a += wf[pat[i] - 1][pat[i + 1] - 1]
-    ret = min(ret, a)
-print(ret)
+l = csgraph.dijkstra(csr_matrix((c, (a, b)), [N + 1] * 2), 0)
+print(min(sum(int(l[i][j]) for i, j in zip(a[:-1], a[1:])) for a in permutations(r)))
