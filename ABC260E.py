27fb9814fd8a5e11295ed7aad37c149f80bf09cc
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-07-23 06:45:18 +0900

    公式

--- ABC260E.py
+++ ABC260E.py
@@ -1,11 +1,14 @@
 N, M = map(int, input().split())
 from collections import defaultdict, deque
+from itertools import accumulate
 
+A = []
+B = []
 G = defaultdict(list)
 for i in range(N):
     a, b = map(int, input().split())
     G[a].append(b)
-    G[b].append(a)
+    A.append(a)
 
 G2 = {}
 for i in range(1, M + 1):
@@ -15,28 +18,29 @@ for i in range(1, M + 1):
         G2[i] = (M, 1)
 
 
-ret = [0] * (M + 1)
+imos = [0] * (M + 2)
 seen = set()
 
+l = 1
+mr = max(A)
+ml = M + 1
+for l in range(1, M + 1):
+    if l > ml:
+        break
+    imos[mr - l + 1] += 1
+    imos[M - l + 2] -= 1
+    for nxt in G[l]:
+        mr = max(mr, nxt)
+        ml = min(ml, nxt)
 
-q = deque()
-q.append((1, M))
-while q:
-    s, t = q.pop()
-    if (s, t) in seen:
-        continue
-    seen.add((s, t))
-    ret[t - s + 1] += 1
-    if s == t:
-        continue
-    if s + 1 <= G2[s][0] and G2[s][1] <= t:
-        q.append((s + 1, t))
-    if s <= G2[t][0] and G2[t][1] <= t - 1:
-        q.append((s, t - 1))
 
-print(*ret[1:])
+ret = list(accumulate(imos))
+
+print(*ret[1:-1])
 
 
 """
 方針誤りなり
+
+単調性に気づき、imos+尺取りを思いつくことが必要
 """
