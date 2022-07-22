e07dca597c9472f6df2657aa6448290979728921
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-07-22 17:03:47 +0900

    公式imos実装

--- python/other/ABC080D.py
+++ python/other/ABC080D.py
@@ -1,27 +1,33 @@
-N, C = map(int, input().split())
+from itertools import accumulate
 
-STC = []
+
+N, C = map(int, input().split())
+RANGE = 10**5
+used = [[0] * (RANGE + 1) for _ in range(C + 1)]
+
 for i in range(N):
     s, t, c = map(int, input().split())
-    STC.append((t, s, c))
-STC.sort()
-INF = 10**18
-occupied = [0] * C
+    used[c][s - 1] += 1
+    used[c][t] -= 1
 
-for t, s, c in STC:
-    target = -1
-    latest = -1
-    for i in range(C):
-        if latest < occupied[i] < s:
-            target = i
-            latest = occupied[i]
-    occupied[target] = t
+for c in range(C + 1):
+    for i in range(RANGE):
+        used[c][i + 1] += used[c][i]
 
-print(len(occupied) - occupied.count(0))
+ret = 0
+for i in range(RANGE):
+    r = 0
+    for c in range(C + 1):
+        r += min(1, used[c][i])
+    ret = max(ret, r)
+
+print(ret)
 
 
 """
 自力AC
 自分の解法は区間スケジューリングの応用
 解説は全部imosっぽい
+
+imosでも実装してみる
 """
