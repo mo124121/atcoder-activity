d3ce111598727c6b5cf6a2e70dc0735f25986fe5
mo124121 <101929016+mo124121@users.noreply.github.com>
2022-07-23 08:50:23 +0900

    解説通り

--- python/other/ABC175D.py
+++ python/other/ABC175D.py
@@ -3,43 +3,39 @@ P = list(map(int, input().split()))
 P = [p - 1 for p in P]
 C = list(map(int, input().split()))
 
-BIT = 30
-dbl = [[0] * (N) for _ in range(BIT + 1)]
-warp = [[0] * (N) for _ in range(BIT + 1)]
-warp[0] = list(range(N))
-warp[1] = P
-
-for i in range(N):
-    dbl[1][i] = C[P[i]]
-
-for bit in range(1, BIT):
-    for i in range(N):
-        warp[bit + 1][i] = warp[bit][warp[bit][i]]
-        dbl[bit + 1][i] = dbl[bit][i] + dbl[bit][warp[bit][i]]
-
-
 INF = 10**18
-ret = max(C)
-for i in range(N):
-    prev = i
-    k = K
-    r = 0
-    flag = False
-    for bit in range(1, BIT + 1):
-        if k >> (bit - 1) & 1:
-            if dbl[bit][prev] > 0:
-                r += dbl[bit][prev]
-                prev = warp[bit][prev]
-                flag = True
-            else:
-                k = (1 << bit) - 1
-    if flag:
-        ret = max(ret, r)
+ret = -INF
 
+for i in range(N):
+    v = i
+    cycle_sum = 0
+    cycle_count = 0
+    while True:
+        cycle_count += 1
+        cycle_sum += C[v]
+        v = P[v]
+        if v == i:
+            break
+    path = 0
+    cnt = 0
+    while True:
+        cnt += 1
+        path += C[v]
+        if cnt > K:
+            break
+        num = (K - cnt) // cycle_count
+        score = path + max(0, cycle_sum) * num
+        ret = max(ret, score)
+        v = P[v]
+        if v == i:
+            break
 
 print(ret)
 
 """
 間違っている
 そもそもDではダブリングでないのでは？
+
+ループとそれ以外で管理して全探索するイメージ
+
 """
